55555f500be68df94b55c501417463c8d07c5623
Seth Perna <gseth26@gmail.com>
2021-12-13 17:18:35 -0500

    wrote model test for updateing status

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 describe 'application show page' do
-  # before do
-  #   @shelter1 = Shelter.create!(foster_program: true, name:" Shelter ", city: "Denver", rank: 2)
-  #   @pet1 = @shelter1.pets.create!(adoptable: true, age: 5, breed:"Pitt Bull", name:"Penelope")
-  #   @pet2 = @shelter1.pets.create!(adoptable: true, age: 3, breed:"Husky X", name:"Lily")
-  #   @pet3 = @shelter1.pets.create!(adoptable: true, age: 3, breed:"Husky X", name:"Bruno")
-  #   @pet4 = @shelter1.pets.create!(adoptable: true, age: 3, breed:"Basset", name:"Herb")
-  #   @application1 = Application.create!(name:'Seth', address: '123 test st', city: 'Denver', state: 'Colorado', zip: '22835' )
-  #   @application_pet = PetApplication.create!(pet_id: @pet1.id, application_id: @application1.id)
-  #   visit "/applications/#{@application1.id}"
-  # end
 
   it ' I see name, address, description, status' do
     @shelter1 = Shelter.create!(foster_program: true, name:" Shelter ", city: "Denver", rank: 2)
